43b2f6bc00f3c9efa0f7c356114a65e0095a0462
Manas <mkadyrbaev@gmail.com>
2020-08-09 16:35:38 +0200

    Update

--- source.py
+++ source.py
@@ -1,5 +1,9 @@
+#Simple Web Scraping
+
 import requests
 from bs4 import BeautifulSoup as soup
-import urllib.request
 
-#1
+path = requests.get("https://news.ycombinator.com/news")
+soupObj = soup(path.text, "html.parser")
+links = (soupObj.select(".storylink")) #grabbing all links
+votes = (soupObj.select(".score")) #grabbing scores
