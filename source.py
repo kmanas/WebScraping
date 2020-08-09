074565a46c7e0d5a5db2fb1b76b4066692d59413
Manas <mkadyrbaev@gmail.com>
2020-08-09 17:15:00 +0200

    Update

--- source.py
+++ source.py
@@ -7,3 +7,17 @@ path = requests.get("https://news.ycombinator.com/news")
 soupObj = soup(path.text, "html.parser")
 links = (soupObj.select(".storylink")) #grabbing all links
 votes = (soupObj.select(".score")) #grabbing scores
+
+def news_by_votes(links, subtext):
+  news = []
+
+  for i, item, in enumerate(links):
+    title = links[i].getText()
+    href = links[i].get("href", None)
+    vote = subtext[i].select(".score")
+    
+    if len(vote):
+      points = int(vote[0].getText().replace(" points", ""))
+      news.append({"Title": title, "Link": href, "votes": points})
+
+    return news
